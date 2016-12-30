a1c8f941db9c9fa1a432f6207c4368b126a2e93f
Mohamed Eldesouki <disooqi@gmail.com>
2016-12-30 21:15:39 +0300

    creating the word page for default annotation

--- annotation/urls.py
+++ annotation/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^(?P<page_num>[0-9]+)/$', views.index, name='index'),
+    url(r'^token/default-labels/(?P<token_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^ambiguous-token/(?P<token_id>[0-9]+)/$', views.ambiguous, name='ambiguous'),
 ]
--- annotation/views.py
+++ annotation/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
-from .models import Token
+from .models import Token, TokenOccurrence
 # Create your views here.
 
 
@@ -10,3 +11,20 @@ def index(request, page_num):
 
     context = {'most_common_tokens': most_common_tokens, 'page': p}
     return render(request, 'annotation/index.html', context)
+
+
+def detail(request, token_id):
+    token = get_object_or_404(Token, pk=token_id)
+    #occurrences = token.tweet_set.all()
+    tweet_occurPosition_list = list()
+    #for tweet in occurrences:
+    token_occur_QuerySet = TokenOccurrence.objects.filter(token=token)
+    for occur in token_occur_QuerySet:
+        tweet_occurPosition_list.append((occur.tweet.tweet_text, occur.position))
+
+    return render(request, 'annotation/detail.html', {'token': token, 'occurrences':tweet_occurPosition_list})
+
+
+def ambiguous(request, token_id):
+    pass
+
