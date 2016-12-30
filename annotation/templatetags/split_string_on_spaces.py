438f2b4ccdbd9cf5874053b322701208f94b5c39
Mohamed Eldesouki <disooqi@gmail.com>
2016-12-30 21:14:22 +0300

    creating a custom templete tag and filter

---
+++ annotation/templatetags/split_string_on_spaces.py
@@ -0,0 +1,20 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+
+@register.filter(needs_autoescape=True)
+@stringfilter
+def add_code_tag(value, arg, autoescape=True):
+    tokens = value.split()
+    if autoescape:
+        esc = conditional_escape
+    else:
+        esc = lambda x: x
+    #tokens[arg] = '<code>'+tokens[arg]+'</code>'
+    tokens[arg] = "<strong><span class=text-info>%s</span></strong>" % (esc(tokens[arg]))
+    return mark_safe(' '.join(tokens))
+    #return ' '.join(tokens)
